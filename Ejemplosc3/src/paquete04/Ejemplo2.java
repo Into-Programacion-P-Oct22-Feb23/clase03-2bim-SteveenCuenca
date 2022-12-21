1be59401b04c375d7aff1a082973501b5a3984ef
626tellor@gmail.com <jacoronel10@utpl.edu.ec>
2022-12-21 03:53:34 -0500

    tercera clase

--- Ejemplosc3/src/paquete04/Ejemplo2.java
+++ Ejemplosc3/src/paquete04/Ejemplo2.java
@@ -16,12 +16,31 @@ public class Ejemplo2 {
      */
     public static void main(String[] args) {
         // Creación de arreglo bidimensionales
-        
-        double[][] dato1 = {{1, 2, 3},{6, 8, 9}}; //
+        String[][] datof = new String [2][3];
+        double[][] dato1 = {{1, 2, 3},{6, 8, 9}};
+        int operacion;
+        double valor;
         /*
             1   4   3
             36  64  9
         */
+        for(int fila = 0; fila < dato1.length; fila++){
+            for (int col = 0; col < dato1[fila].length; col++){
+                valor = dato1[fila][col];
+                if (valor%2 == 0){
+                    operacion = (int) (dato1[fila][col] * dato1[fila][col]);
+                }else{
+                    operacion = (int) (dato1[fila][col] * 1);
+                }           
+                datof[fila][col] = String.format("%d", operacion);
+            }
+        }
+        for(int fila = 0; fila < dato1.length; fila++){
+            for (int col = 0; col < dato1[fila].length; col++){
+                System.out.printf("%s\t", datof[fila][col]);
+            }
+            System.out.println("");
+        }
         
     }
     
